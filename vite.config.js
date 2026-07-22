b6286bd42212b6e7afbef8321b3ea78d3c753fb2
Treasure123-school <treasurehomeschool@gmail.com>
2026-07-22 22:03:05 +0100

    make vite work in replit and on acode

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  server: {
-    host: '0.0.0.0',
-    port: 5000,
-    allowedHosts: true,
-  },
+
+  ...(process.env.REPL_ID
+    ? {
+        server: {
+          host: '0.0.0.0',
+          port: 5000,
+          allowedHosts: true,
+        },
+      }
+    : {}),
 })
