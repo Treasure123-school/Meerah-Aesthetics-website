915e8d68fd4e9e22b3d353eb093976361ab111c3
Treasure123-school <treasurehomeschool@gmail.com>
2026-07-23 13:46:09 +0100

    added bg hero utility class to index.css

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -59,7 +59,7 @@ const Hero = () => {
 
 			<div
 				className="relative flex-1 flex justify-center items-center
-				xl:min-h-screen max-xl:py-40 bg-white bg-hero bg-cover bg-center"
+				xl:min-h-screen max-xl:py-40 bg-white bg-hero bg-cover bg-center "
 			>
 				<img
 					src={bigShoe1}
